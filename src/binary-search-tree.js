5e919e610eb2115353148f1bde7453367d0518d0
Yorokay <yorokay05110@mail.ru>
2023-12-22 23:36:26 +0300

    feat: solve binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,196 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+
 class BinarySearchTree {
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor() {
+		this.tree = new Node(null);
+		this.currentNode = this.tree;
+		this.preventNode = null;
+	}
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	root() {
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+		return this.tree.data === null ?
+			null : this.tree;
+	}
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	add(data) {
+		let newNode = new Node(data);
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+		if (this.tree.data === null) {
+			this.tree = newNode;
+			this.currentNode = this.tree;
+		}
+		else if (this.currentNode.left === null && data < this.currentNode.data) {
+			this.currentNode.left = newNode;
+			this.currentNode = this.tree;
+		}
+		else if (this.currentNode.right === null && data > this.currentNode.data) {
+			this.currentNode.right = newNode;
+			this.currentNode = this.tree;
+		}
+		else {
+			if (data < this.currentNode.data) {
+				this.currentNode = this.currentNode.left;
+				this.add(data);
+			}
+			else if (data > this.currentNode.data) {
+				this.currentNode = this.currentNode.right;
+				this.add(data);
+			}
+		}
+	}
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	has(data) {
+		let hasNode = this.find(data);
+		return hasNode === null ? false : true;
+	}
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	find(data) {
+		let foundNode = null;
+
+		if (this.currentNode.data === data) {
+			foundNode = this.currentNode;
+		}
+		else {
+			if (data < this.currentNode.data && this.currentNode.left !== null) {
+				this.preventNode = this.currentNode;
+				this.currentNode = this.currentNode.left;
+				foundNode = this.find(data);
+			}
+			else if (data > this.currentNode.data && this.currentNode.right !== null) {
+				this.preventNode = this.currentNode;
+				this.currentNode = this.currentNode.right;
+				foundNode = this.find(data);
+			}
+		}
+
+		this.currentNode = this.tree;
+		return foundNode;
+	}
+
+	remove(data) {
+		let removeNode = this.find(data);
+		let minMaxNode;
+		let minMaxPreventNode = removeNode;
+
+		if (removeNode.left === null && removeNode.right === null) {
+
+			if (removeNode === this.tree) {
+				this.tree.data = null;
+			}
+			else if (removeNode.data < this.preventNode.data) {
+				this.preventNode.left = null;
+			}
+			else {
+				this.preventNode.right = null;
+			}
+		}
+		else if (removeNode.left !== null) {
+			minMaxNode = removeNode.left;
+
+			while (minMaxNode.right !== null) {
+				minMaxPreventNode = minMaxNode;
+				minMaxNode = minMaxNode.right;
+			}
+
+			if (minMaxPreventNode === removeNode) {
+
+				if (minMaxNode.left === null) {
+					minMaxPreventNode.left = null
+				} else {
+					minMaxPreventNode.left = minMaxNode.left;
+				}
+			}
+			else {
+
+				if (minMaxNode.left === null) {
+					minMaxPreventNode.right = null
+				} else {
+					minMaxPreventNode.right = minMaxNode.left;
+				}
+			}
+
+			removeNode.data = minMaxNode.data;
+		}
+		else {
+			minMaxNode = removeNode.right;
+
+			while (minMaxNode.left !== null) {
+				minMaxPreventNode = minMaxNode;
+				minMaxNode = minMaxNode.left;
+			}
+
+			if (minMaxPreventNode === removeNode) {
+
+				if (minMaxNode.right === null) {
+					minMaxPreventNode.right = null
+				} else {
+					minMaxPreventNode.right = minMaxNode.right;
+				}
+			}
+			else {
+
+				if (minMaxNode.right === null) {
+					minMaxPreventNode.left = null
+				} else {
+					minMaxPreventNode.left = minMaxNode.right;
+				}
+			}
+
+			removeNode.data = minMaxNode.data;
+		}
+		this.preventNode = null;
+	}
+
+	min() {
+		let minNode;
+
+		if (this.tree.data === null) {
+			return null;
+		}
+		else if (this.currentNode.left === null) {
+			minNode = this.currentNode.data;
+			this.currentNode = this.tree;
+			return minNode;
+		}
+		else {
+			this.currentNode = this.currentNode.left;
+			minNode = this.min();
+			this.currentNode = this.tree;
+			return minNode;
+		}
+	}
+
+	max() {
+		let maxNode;
+
+		if (this.tree.data === null) {
+			return null;
+		}
+		else if (this.currentNode.right === null) {
+			maxNode = this.currentNode.data;
+			this.currentNode = this.tree;
+			return maxNode;
+		}
+		else {
+			this.currentNode = this.currentNode.right;
+			maxNode = this.max();
+			this.currentNode = this.tree;
+			return maxNode;
+		}
+	}
 }
 
 module.exports = {
-  BinarySearchTree
+	BinarySearchTree
 };
+
--- src/queue.js
+++ src/queue.js
@@ -47,14 +47,3 @@ module.exports = {
 	Queue
 };
 
-let queue = new Queue();
-
-// queue.enqueue(1)
-// queue.enqueue(2)
-// queue.enqueue(3)
-
-// console.log(queue.getUnderlyingList())
-// queue.dequeue();
-// console.log(queue.arr)
-
-// console.log(queue.dequeue());
